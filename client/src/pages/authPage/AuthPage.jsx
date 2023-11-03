3c12a8a4a78c973393389e6ed2a5815af85bf738
mnoumanali001 <nouman.qayyum@devsinc.com>
2023-11-04 02:41:26 +0500

    setup auth page

---
+++ client/src/pages/authPage/AuthPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AuthForm from "../../components/form/AuthForm";
+import "./authpage.css";
+function AuthPage() {
+  return (
+    <div className="auth-page-container">
+      <AuthForm />
+    </div>
+  );
+}
+
+export default AuthPage;
