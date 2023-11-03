7a1e5e650257ad8f540cf31e641ef08cd5faf231
mnoumanali001 <nouman.qayyum@devsinc.com>
2023-11-04 02:42:08 +0500

    create checkout page

---
+++ client/src/pages/checkoutPage/CheckoutPage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Burgur from "../../components/burgur/Burgur";
+import OrderForm from "../../components/form/OrderForm";
+import "./checkoutPage.css";
+import { Link, Outlet } from "react-router-dom";
+
+function CheckoutPage() {
+  return (
+    <div className="checkout-page-container">
+      <Burgur />
+      <div className="checkout-option">
+        <Link className="cancel-order" to="/">
+          Cancel
+        </Link>
+        <Link className="continue-order" to="contact-data">
+          Continue
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
+export default CheckoutPage;
