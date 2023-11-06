43100caad4fc152e8eb56f99896662595b18cc1b
mnoumanali001 <nouman.qayyum@devsinc.com>
2023-11-07 02:44:26 +0500

    modify initial state

--- client/src/context/AppContextProvider.jsx
+++ client/src/context/AppContextProvider.jsx
@@ -5,10 +5,32 @@ export const AppContext = createContext();
 
 const AppContextProvider = ({ children }) => {
   const [burgurPrice, setBurgurPrice] = useState(4);
-  const [burgurOpt, setBurgurOpt] = useState(burgurOptions);
+  const [burgurOpt, setBurgurOpt] = useState([
+    {
+      name: "Salad",
+      price: 0.5,
+      count: [],
+    },
+    {
+      name: "Bacon",
+      price: 0.7,
+      count: [],
+    },
+    {
+      name: "Cheese",
+      price: 0.4,
+      count: [],
+    },
+    {
+      name: "Meat",
+      price: 1.3,
+      count: [],
+    },
+  ]);
+  const [orders, setOrder] = useState([]);
   return (
     <AppContext.Provider
-      value={{ burgurPrice, burgurOpt, setBurgurOpt, setBurgurPrice }}
+      value={{ burgurPrice, burgurOpt, setBurgurOpt, setBurgurPrice, orders, setOrder }}
     >
       {children}
     </AppContext.Provider>
