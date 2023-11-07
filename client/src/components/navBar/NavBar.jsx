71e509d3af133bc6e8b6a39b94a4a67bd7471971
mnoumanali001 <nouman.qayyum@devsinc.com>
2023-11-07 22:42:00 +0500

    modify navbar style

--- client/src/components/navBar/NavBar.jsx
+++ client/src/components/navBar/NavBar.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 import "./navbar.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { deleteLoggedUser, getLoggedUser } from "../../utils/user";
 
 function NavBar() {
+  const data = getLoggedUser();
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    deleteLoggedUser();
+    navigate("/");
+  };
   return (
     <div className="navbar-container">
       <img src={logo} alt="icon" />
-      <nav>
+      <nav style={{width : data ? "20%" : "15%"}}>
         <Link to="/" className="navbar-link">
           Burgur Builder
         </Link>
-        <Link to="/auth" className="navbar-link">
-          Sign In
-        </Link>
+        {data ? (
+          <Link to="/orders" className="navbar-link">
+            Orders
+          </Link>
+        ) : null}
+        {data ? (
+          <p className="navbar-link" onClick={handleLogout}>
+            Logout
+          </p>
+        ) : (
+          <Link to="/auth" className="navbar-link">
+            Sign In
+          </Link>
+        )}
       </nav>
     </div>
   );
